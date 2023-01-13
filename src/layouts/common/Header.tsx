bdc15d6c9c4fd37a6d82c744381390845b22398e
lokesh <lokeshpatwal14@gmail.com>
2023-01-13 18:36:12 +0530

    responsive

--- src/layouts/common/Header.tsx
+++ src/layouts/common/Header.tsx
@@ -3,32 +3,32 @@ import SearchBar from "../../components/Search";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import { ColorButton } from "../../components/Button";
+import Grid from "@mui/material/Grid/Grid";
 
 
 const Header = () => {
   return (
-    <Box sx={{ display: "flex", justifyContent: "space-around", position: 'relative', top: "-2rem" }}>
+    <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", position: 'relative', top: "-2rem" }}>
       <Box>
-        <SearchBar/>
+        <SearchBar />
       </Box>
-      <Stack direction="row" spacing={2}>
-        <ColorButton variant="contained" startIcon={<AddCircleIcon />}>
+      <Stack direction="row" spacing={2} display="flex" alignItems="center">
+        <ColorButton variant="contained" startIcon={<AddCircleIcon />} sx={{ height: 'fit-content' }}>
           Create
         </ColorButton>
-        <ColorButton
-          variant="contained"
-          // sx={{ ml: 1 }}
-          startIcon={<AddCircleIcon />}
-        >
+        <ColorButton variant="contained" startIcon={<AddCircleIcon />} sx={{
+          display: { xs: 'none', md: 'flex', lg: 'flex', xl: 'flex' },
+          height: 'fit-content'
+        }}>
           Ethereum Chain
         </ColorButton>
-        <ColorButton
-          variant="contained"
-          // sx={{ ml: 1 }}
-          startIcon={<AccountBalanceWalletIcon />}
-        >
-          {" "}
-          Connect wallet
+        <ColorButton variant="contained" startIcon={<AddCircleIcon />} sx={{
+          display: { xs: 'block', md: 'none', lg: 'none', xl: 'none' },
+          height: 'fit-content'
+        }}>ETH
+        </ColorButton>
+        <ColorButton variant="contained" startIcon={<AccountBalanceWalletIcon />} sx={{ height: 'fit-content' }}>
+          {" "} Connect wallet
         </ColorButton>
       </Stack>
     </Box>
